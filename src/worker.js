9d1478c89d48f848b688ef690c4547ef3875bb2b
Michael Esteban <mickel13@gmail.com>
2024-12-01 17:08:43 +1000

    add credit

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,5 @@
+// See https://github.com/huggingface/transformers.js-examples/blob/main/smollm-webgpu/src/worker.js for the original source code. Licensed under Apache 2.0.
+
 import {
   AutoTokenizer,
   AutoModelForCausalLM,
@@ -29,14 +31,10 @@ const stopping_criteria = new InterruptableStoppingCriteria();
 async function generate(messages) {
   const [tokenizer, model] = await TextGenerationPipeline.getInstance();
 
-  console.log("messages", messages);
-
   const inputs = tokenizer.apply_chat_template(messages, {
     add_generation_prompt: true,
     return_dict: true,
   });
-
-  console.log("inputs", inputs);
 
   let startTime;
   let numTokens = 0;
