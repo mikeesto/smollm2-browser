274dac4b439abf573dbb1c5589ab6dc01a15267e
Michael Esteban <mickel13@gmail.com>
2024-12-01 17:14:39 +1000

    remove unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import { ModelLoader } from "@/components/ModelLoader";
 import { useWorker } from "./context/workerContext";
 import { Chat } from "@/components/Chat";
 
 export default function App() {
-  const [progressItems, setProgressItems] = useState<ProgressItem[]>([]);
   const { status } = useWorker();
 
   return <>{status === "ready" ? <Chat /> : <ModelLoader />}</>;
